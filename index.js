cf7b3d1399a4b3966c29c80f20fd3b877f8f1339
piopod <piotr@piotrpodlas.pl>
2017-12-23 14:42:43 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-var OSinfo = require('./moduls/OSinfo');
+var fs = require('fs');
+var colors = require("colors");
 
-process.stdin.setEncoding("utf-8");
-process.stdin.on("readable", function(){
-    var input = process.stdin.read();
-    if(input!== null){
-        var instruction = input.toString().trim();
-        switch(instruction){
-            case "/exit":
-                process.stdout.write("Quitting app!\n");
-                process.exit();
-                break;
-            case "lang":
-                process.stdout.write(process.env.LANG);
-                break;
-            case "version":
-                process.stdout.write(process.versions.node);
-                break;
-            case '/getOSinfo':
-                OSinfo.print();
-                break;
-            default:
-                process.stderr.write("Wrong instruction!\n");
-        }
-    }
+fs.readdir("./doczytania", "utf-8", function (err, files){
+    console.log(files);
+
+    fs.writeFile("./poczytaniupo.txt",files, function(err){
+        if (err) throw err;
+        console.log("Zapisano!".blue);
+    });
+
 });
