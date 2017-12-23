8df50d6a7367d333bba6b0575f6d5cfb318b9107
piopod <piotr@piotrpodlas.pl>
2017-12-23 13:50:15 +0100

    Add files via upload

---
+++ kodilla_node_1.3.js
@@ -0,0 +1,22 @@
+process.stdin.setEncoding("utf-8");
+process.stdin.on("readable", function(){
+    var input = process.stdin.read();
+    if(input!== null){
+        var instruction = input.toString().trim();
+        switch(instruction){
+            case "/exit":
+                process.stdout.write("Quitting app!\n");
+                process.exit();
+                break;
+            case "lang":
+                process.stdout.write(String(process.env.LANG));
+                break;
+            case "version":
+                process.stdout.write(process.versions.node);
+                break;
+            default:
+                process.stderr.write("Wrong instruction!\n");
+
+        }
+    }
+});
