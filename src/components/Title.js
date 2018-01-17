b67e7ae64fa343e74a3d2cee0512d6441a912718
piopod <piotr@piotrpodlas.pl>
2018-01-17 20:58:06 +0100

    Add files via upload

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+var Title = React.createClass({
+  propTypes: {
+    content: React.PropTypes.string.isRequired
+  },
+  
+  render: function() {
+    return (
+      React.createElement('h1', {}, 'To do app. 10 zadań')
+    )
+  }
+});
+
+// export default console.log;
